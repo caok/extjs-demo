f15d6b04ec73a57e8b67dce215b773c3b4568f99
caok <caok1231@gmail.com>
2013-06-18 16:16:22 +0800

    modify routes

--- app.js
+++ app.js
@@ -5,7 +5,6 @@
 
 var express = require('express')
   , routes = require('./routes')
-  , user = require('./routes/user')
   , http = require('http')
   , path = require('path');
 
@@ -27,8 +26,7 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-app.get('/', routes.index);
-app.get('/users', user.list);
+routes(app);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,31 @@
-
-/*
- * GET home page.
- */
+var Article = require('../models/article.js');
 
 exports.index = function(req, res){
   res.render('index', { title: 'Express' });
 };
+
+module.exports = function (app) {
+  // 主页
+  app.get('/', function(req, res){
+    Article.get(null, function(err, articles){
+      if(err){
+        articles = [];
+      }
+      res.render('index', {
+        title    : '主页',
+        articles : articles
+      });
+    });
+  });
+
+  // 文章清单
+  app.get('/articles', function(req, res) {
+    Article.get(null, function(err, articles){
+      if(err){
+        articles = [];
+      }
+      res.contentType('json');
+      res.json({success: true, articles: articles});
+    });
+  });
+}
